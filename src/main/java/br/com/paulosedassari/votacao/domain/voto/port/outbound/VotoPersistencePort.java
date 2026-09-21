190344411ffcbd464c70c293beb5ac7a69447797
paulosedassari <pssantos.ph200@gmail.com>
2026-09-21 11:14:28 -0300

    feat: abstracao para dominio persistir voto

---
+++ src/main/java/br/com/paulosedassari/votacao/domain/voto/port/outbound/VotoPersistencePort.java
@@ -0,0 +1,10 @@
+package br.com.paulosedassari.votacao.domain.voto.port.outbound;
+
+import br.com.paulosedassari.votacao.domain.voto.model.Voto;
+
+public interface VotoPersistencePort {
+
+    boolean existePorPautaIdEAssociadoId(Long pautaId, String associadoId);
+
+    Voto salvar(Voto voto);
+}
