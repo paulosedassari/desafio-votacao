e9a3be6a4270b7862a7381beb2eeffc14c818c26
paulosedassari <pssantos.ph200@gmail.com>
2026-09-20 22:44:41 -0300

    feat: criando bean da camada de dominio
    domain nao deve depender de framework

---
+++ src/main/java/br/com/paulosedassari/votacao/infra/config/PautaConfig.java
@@ -0,0 +1,19 @@
+package br.com.paulosedassari.votacao.infra.config;
+
+import java.time.Clock;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.paulosedassari.votacao.domain.pauta.port.inbound.CadastrarPautaUseCase;
+import br.com.paulosedassari.votacao.domain.pauta.port.outbound.PautaPersistencePort;
+import br.com.paulosedassari.votacao.domain.pauta.service.CadastrarPautaService;
+
+@Configuration
+public class PautaConfig {
+
+	@Bean
+	CadastrarPautaUseCase cadastrarPauta(PautaPersistencePort persistencePort, Clock relogio) {
+		return new CadastrarPautaService(persistencePort, relogio);
+	}
+}
