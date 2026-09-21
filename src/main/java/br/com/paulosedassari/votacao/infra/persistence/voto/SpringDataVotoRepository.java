a6dfee15237ecdb1cde800650476f5a79e7062dd
paulosedassari <pssantos.ph200@gmail.com>
2026-09-21 19:58:57 -0300

    refactor: sql para contabilizacao dos votos

--- src/main/java/br/com/paulosedassari/votacao/infra/persistence/voto/SpringDataVotoRepository.java
+++ src/main/java/br/com/paulosedassari/votacao/infra/persistence/voto/SpringDataVotoRepository.java
@@ -1,8 +1,20 @@
 package br.com.paulosedassari.votacao.infra.persistence.voto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 interface SpringDataVotoRepository extends JpaRepository<VotoJpaEntity, Long> {
 
     boolean existsByPautaIdAndAssociadoId(Long pautaId, String associadoId);
+
+    @Query(value = """
+            SELECT valor AS valor, COUNT(*) AS quantidade
+            FROM voto
+            WHERE pauta_id = :pautaId
+            GROUP BY valor
+            """, nativeQuery = true)
+    List<ContagemVotosProjection> contabilizarPorPautaId(@Param("pautaId") Long pautaId);
 }
