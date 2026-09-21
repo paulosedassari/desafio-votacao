474b76ab22e4c50fcd9993ae02ab16e868b2c7de
paulosedassari <pssantos.ph200@gmail.com>
2026-09-20 22:34:03 -0300

    feat: documentacao swagger ui para pauta

---
+++ src/main/java/br/com/paulosedassari/votacao/interfaces/web/pauta/PautaOpenApi.java
@@ -0,0 +1,42 @@
+package br.com.paulosedassari.votacao.interfaces.web.pauta;
+
+import br.com.paulosedassari.votacao.interfaces.web.error.ErroResponse;
+import br.com.paulosedassari.votacao.interfaces.web.pauta.dto.PautaRequest;
+import br.com.paulosedassari.votacao.interfaces.web.pauta.dto.PautaResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "Pautas", description = "Operações relacionadas às pautas de votação")
+@RequestMapping("/api/v1/pautas")
+public interface PautaOpenApi {
+
+    @Operation(summary = "Cadastrar pauta", description = "Cadastra uma nova pauta de votação")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Pauta cadastrada com sucesso"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Corpo da requisição inválido",
+                    content = @Content(schema = @Schema(implementation = ErroResponse.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Recurso não encontrado",
+                    content = @Content(schema = @Schema(implementation = ErroResponse.class))),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Falha interna",
+                    content = @Content(schema = @Schema(implementation = ErroResponse.class)))
+    })
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<PautaResponse> criarPauta(
+            @Valid @RequestBody PautaRequest pautaRequest);
+}
