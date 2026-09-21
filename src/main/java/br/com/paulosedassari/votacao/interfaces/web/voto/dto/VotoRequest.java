a684b2550ae722416a4af0c45f804b2956a2eddd
paulosedassari <pssantos.ph200@gmail.com>
2026-09-21 14:57:07 -0300

    refactor: criando pattern para validar voto valido na borda

--- src/main/java/br/com/paulosedassari/votacao/interfaces/web/voto/dto/VotoRequest.java
+++ src/main/java/br/com/paulosedassari/votacao/interfaces/web/voto/dto/VotoRequest.java
@@ -3,7 +3,7 @@ package br.com.paulosedassari.votacao.interfaces.web.voto.dto;
 import br.com.paulosedassari.votacao.domain.voto.model.ValorVoto;
 import br.com.paulosedassari.votacao.domain.voto.port.inbound.RegistrarVotoCommand;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record VotoRequest(
@@ -12,11 +12,12 @@ public record VotoRequest(
         @Size(max = 100, message = "O identificador do associado deve possuir no máximo 100 caracteres.")
         String associadoId,
 
-        @NotNull(message = "O voto é obrigatório.")
-        ValorVoto voto
+        @NotBlank(message = "O voto é obrigatório.")
+        @Pattern(regexp = "SIM|NAO", message = "O voto deve ser SIM ou NAO.")
+        String voto
 ) {
 
     public RegistrarVotoCommand toCommand(Long pautaId) {
-        return new RegistrarVotoCommand(pautaId, associadoId, voto);
+        return new RegistrarVotoCommand(pautaId, associadoId, ValorVoto.valueOf(voto));
     }
 }
