4008298ef44fcb78462c7320da317caab924972d
paulosedassari <pssantos.ph200@gmail.com>
2026-09-20 22:43:32 -0300

    feat: abstracao para dominio persistir pauta

---
+++ src/main/java/br/com/paulosedassari/votacao/domain/pauta/port/outbound/PautaPersistencePort.java
@@ -0,0 +1,8 @@
+package br.com.paulosedassari.votacao.domain.pauta.port.outbound;
+
+import br.com.paulosedassari.votacao.domain.pauta.model.Pauta;
+
+public interface PautaPersistencePort {
+
+	Pauta salvar(Pauta pauta);
+}
