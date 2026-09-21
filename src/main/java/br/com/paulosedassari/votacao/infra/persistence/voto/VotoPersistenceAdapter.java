98e98800672ded1f6b7cfac198f42e0bbfc42c32
paulosedassari <pssantos.ph200@gmail.com>
2026-09-21 11:15:33 -0300

    feat: adapter para implementar persistência do voto

---
+++ src/main/java/br/com/paulosedassari/votacao/infra/persistence/voto/VotoPersistenceAdapter.java
@@ -0,0 +1,48 @@
+package br.com.paulosedassari.votacao.infra.persistence.voto;
+
+import br.com.paulosedassari.votacao.domain.voto.exception.VotoDuplicadoException;
+import br.com.paulosedassari.votacao.domain.voto.model.Voto;
+import br.com.paulosedassari.votacao.domain.voto.port.outbound.VotoPersistencePort;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+class VotoPersistenceAdapter implements VotoPersistencePort {
+
+    private final SpringDataVotoRepository repository;
+
+    @Override
+    public boolean existePorPautaIdEAssociadoId(Long pautaId, String associadoId) {
+        return repository.existsByPautaIdAndAssociadoId(pautaId, associadoId);
+    }
+
+    @Override
+    public Voto salvar(Voto voto) {
+        try {
+            var entidade = new VotoJpaEntity(
+                    voto.id(),
+                    voto.pautaId(),
+                    voto.associadoId(),
+                    voto.valor(),
+                    voto.criadoEm()
+            );
+
+            var entidadeSalva = repository.saveAndFlush(entidade);
+            log.info("Voto registrado. votoId={} pautaId={}", entidadeSalva.getId(), voto.pautaId());
+
+            return new Voto(
+                    entidadeSalva.getId(),
+                    entidadeSalva.getPautaId(),
+                    entidadeSalva.getAssociadoId(),
+                    entidadeSalva.getValor(),
+                    entidadeSalva.getCriadoEm()
+            );
+        } catch (DataIntegrityViolationException exception) {
+            throw new VotoDuplicadoException();
+        }
+    }
+}
