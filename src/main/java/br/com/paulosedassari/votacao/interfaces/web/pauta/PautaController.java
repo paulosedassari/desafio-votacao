cbeb1163cf17968f81e27b0b647d589b34a73724
paulosedassari <pssantos.ph200@gmail.com>
2026-09-21 22:39:54 -0300

    refactor: comunicacao de borda para listar pautas

--- src/main/java/br/com/paulosedassari/votacao/interfaces/web/pauta/PautaController.java
+++ src/main/java/br/com/paulosedassari/votacao/interfaces/web/pauta/PautaController.java
@@ -1,6 +1,7 @@
 package br.com.paulosedassari.votacao.interfaces.web.pauta;
 
 import br.com.paulosedassari.votacao.domain.pauta.port.inbound.CadastrarPautaUseCase;
+import br.com.paulosedassari.votacao.domain.pauta.port.inbound.ListarPautasUseCase;
 import br.com.paulosedassari.votacao.interfaces.web.pauta.dto.PautaRequest;
 import br.com.paulosedassari.votacao.interfaces.web.pauta.dto.PautaResponse;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class PautaController implements PautaOpenApi {
 
     private final CadastrarPautaUseCase cadastrarPautaUseCase;
+    private final ListarPautasUseCase listarPautasUseCase;
 
     @Override
     public ResponseEntity<PautaResponse> criarPauta(PautaRequest pautaRequest) {
@@ -21,5 +25,13 @@ public class PautaController implements PautaOpenApi {
         var response = PautaResponse.toResponse(pautaCriada);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @Override
+    public ResponseEntity<List<PautaResponse>> listarPautas() {
+        var pautas = listarPautasUseCase.executar();
+        var response = PautaResponse.toResponse(pautas);
+
+        return ResponseEntity.ok(response);
     }
 }
