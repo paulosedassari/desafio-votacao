5121647f367c5e170bb35a311408738339e63356
paulosedassari <pssantos.ph200@gmail.com>
2026-09-21 22:40:55 -0300

    refactor: mapeamento lista de pautas do dominio para borda

--- src/main/java/br/com/paulosedassari/votacao/interfaces/web/pauta/dto/PautaResponse.java
+++ src/main/java/br/com/paulosedassari/votacao/interfaces/web/pauta/dto/PautaResponse.java
@@ -1,8 +1,10 @@
 package br.com.paulosedassari.votacao.interfaces.web.pauta.dto;
 
 import br.com.paulosedassari.votacao.domain.pauta.port.inbound.PautaCriada;
+import br.com.paulosedassari.votacao.domain.pauta.port.inbound.PautaListada;
 
 import java.time.Instant;
+import java.util.List;
 
 public record PautaResponse(
         Long id,
@@ -18,5 +20,20 @@ public record PautaResponse(
                 pauta.descricao(),
                 pauta.criadoEm()
         );
+    }
+
+    public static PautaResponse toResponse(PautaListada pauta) {
+        return new PautaResponse(
+                pauta.id(),
+                pauta.titulo(),
+                pauta.descricao(),
+                pauta.criadoEm()
+        );
+    }
+
+    public static List<PautaResponse> toResponse(List<PautaListada> pautas) {
+        return pautas.stream()
+                .map(PautaResponse::toResponse)
+                .toList();
     }
 }
