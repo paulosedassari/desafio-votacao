8d99959049fd4608c9572ba140a0c344b680dabc
paulosedassari <pssantos.ph200@gmail.com>
2026-09-21 22:44:38 -0300

    refactor:  configurado bean para listagem de pautas

--- src/main/java/br/com/paulosedassari/votacao/infra/config/PautaConfig.java
+++ src/main/java/br/com/paulosedassari/votacao/infra/config/PautaConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.paulosedassari.votacao.domain.pauta.port.inbound.CadastrarPautaUseCase;
+import br.com.paulosedassari.votacao.domain.pauta.port.inbound.ListarPautasUseCase;
 import br.com.paulosedassari.votacao.domain.pauta.port.outbound.PautaPersistencePort;
 import br.com.paulosedassari.votacao.domain.pauta.service.CadastrarPautaService;
+import br.com.paulosedassari.votacao.domain.pauta.service.ListarPautasService;
 
 @Configuration
 public class PautaConfig {
@@ -15,5 +17,10 @@ public class PautaConfig {
 	@Bean
 	CadastrarPautaUseCase cadastrarPauta(PautaPersistencePort persistencePort, Clock relogio) {
 		return new CadastrarPautaService(persistencePort, relogio);
+	}
+
+	@Bean
+	ListarPautasUseCase listarPautas(PautaPersistencePort persistencePort) {
+		return new ListarPautasService(persistencePort);
 	}
 }
