8a4dc0f2b2b5397de7e0d9641730b3824f3dee1a
paulosedassari <pssantos.ph200@gmail.com>
2026-09-21 22:42:16 -0300

    refactor: adicionado contrato para implementar logica de listar pautas

--- src/main/java/br/com/paulosedassari/votacao/domain/pauta/port/outbound/PautaPersistencePort.java
+++ src/main/java/br/com/paulosedassari/votacao/domain/pauta/port/outbound/PautaPersistencePort.java
@@ -2,9 +2,13 @@ package br.com.paulosedassari.votacao.domain.pauta.port.outbound;
 
 import br.com.paulosedassari.votacao.domain.pauta.model.Pauta;
 
+import java.util.List;
+
 public interface PautaPersistencePort {
 
 	Pauta salvar(Pauta pauta);
 
 	boolean existePorId(Long pautaId);
+
+	List<Pauta> buscarTodas();
 }
